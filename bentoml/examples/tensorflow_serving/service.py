e695d5784cd5342136cbd83aac502600da2f3a4f
youjin2 <youjin2@snu.ac.kr>
2022-10-08 19:56:42 +0900

    MAINT: bento serving with tensorflow model

--- bentoml/examples/tensorflow_serving/service.py
+++ bentoml/examples/tensorflow_serving/service.py
@@ -22,37 +22,35 @@ svc = bentoml.Service(
 
 
 @svc.api(
-    input=NumpyNdarray(dtype="float32", enforce_dtype=True),
+    input=NumpyNdarray(dtype="uint8", enforce_dtype=True),
     output=NumpyNdarray(dtype="int64"),
 )
-def predict(inp):
-    return 1
+async def predict_ndarray(
+    inp: "np.ndarray[t.Any, np.dtype[t.Any]]",
+) -> "np.ndarray[t.Any, np.dtype[t.Any]]":
+    assert inp.shape == (28, 28)
 
-# @svc.api(
-#     input=NumpyNdarray(dtype="float32", enforce_dtype=True),
-#     output=NumpyNdarray(dtype="int64"),
-# )
-# async def predict_ndarray(
-#     inp: "np.ndarray[t.Any, np.dtype[t.Any]]",
-# ) -> "np.ndarray[t.Any, np.dtype[t.Any]]":
-#     assert inp.shape == (28, 28)
+    # extra channel dimension
+    inp = inp.astype("float32")
+    inp = np.expand_dims(inp, (0, 3)) / 255.
+    output_arr = await mnist_runner.async_run(inp)
+    pred_labels = np.argmax(output_arr, axis=1)
 
-#     # extra channel dimension
-#     inp = np.expand_dims(inp, 2)
-#     output_tensor = await mnist_runner.async_run(inp)
-#     return output_tensor.numpy()
+    return pred_labels
 
 
-# @svc.api(
-#     input=Image(),
-#     output=NumpyNdarray(dtype="int64")
-# )
-# async def predict_image(f: PILImage) -> "np.ndarray[t.Any, np.dtype[t.Any]]":
-#     assert isinstance(f, PILImage)
-#     arr = np.array(f)/255.0
-#     assert arr.shape == (28, 28)
+@svc.api(
+    input=Image(),
+    output=NumpyNdarray(dtype="int64")
+)
+async def predict_image(f: PILImage) -> "np.ndarray[t.Any, np.dtype[t.Any]]":
+    assert isinstance(f, PILImage)
+    arr = np.array(f)/255.0
+    assert arr.shape == (28, 28)
 
-#     # extra channel dimension
-#     arr = np.expand_dims(arr, (0, 3)).astype("float32")
-#     output_tensor = await mnist_runner.async_run(arr)
-#     return output_tensor.numpy()
+    # extra channel dimension
+    arr = np.expand_dims(arr, (0, 3)).astype("float32")
+    output_arr = await mnist_runner.async_run(arr)
+    pred_labels = np.argmax(output_arr, axis=1)
+
+    return pred_labels
