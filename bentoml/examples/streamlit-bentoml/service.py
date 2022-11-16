c7bc32ab659396deab662f1e74e537fb8876b3d3
youjin2 <youjin2@snu.ac.kr>
2022-11-16 23:25:15 +0900

    MAINT: change asynchronous api to synchronized one

--- bentoml/examples/streamlit-bentoml/service.py
+++ bentoml/examples/streamlit-bentoml/service.py
@@ -13,15 +13,11 @@ svc = bentoml.Service(
 )
 
 
-@svc.api(input=Text(), output=NumpyNdarray())
-async def predict_pawpularity_score(buffer):
+def preprocess(image_buffer):
     width, height = (224, 224)
 
-    # print("====="*20)
-    # print(buffer)
-    # print("====="*20)
-    buffer = base64.b64decode(buffer)
-    image = tf.image.decode_jpeg(buffer)
+    image_buffer = base64.b64decode(image_buffer)
+    image = tf.image.decode_jpeg(image_buffer)
     resized = tf.image.resize(
         image,
         size=(width, height),
@@ -30,8 +26,23 @@ async def predict_pawpularity_score(buffer):
     )
     resized /= 255.
     inputs = tf.reshape(tf.cast(resized, "float32"), (1, width, height, 3))
-    scores = await conv2d_runner.async_run({
-        "input_1": inputs
-    })["dense_1"]
+
+    return inputs
+
+
+# @svc.api(input=Text(), output=NumpyNdarray())
+# async def predict_pawpularity_score(buffer):
+#     # TODO: how to use async api for tensorflow==2.4
+#     inputs = preprocess(buffer)
+#     scores = await conv2d_runner.async_run({"input_1": inputs})
+
+#     return scores
+
+
+@svc.api(input=Text(), output=NumpyNdarray())
+def predict_pawpularity_score(buffer):
+    inputs = preprocess(buffer)
+    inputs = tf.concatenate([inputs, inputs])
+    scores = conv2d_runner.run({"input_1": inputs})["dense_1"]
 
     return scores
